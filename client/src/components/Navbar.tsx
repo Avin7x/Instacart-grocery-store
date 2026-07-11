d1a075897144a87d3269a97f80505e6c793085d0
devin567 <avinnash567@gmail.com>
2026-07-11 23:51:16 +0530

    created productPage.tsx, MyOrders.tsx ui components

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -34,7 +34,8 @@ const Navbar = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 gap-4">
         {/* logo */}
        <div>
-         <Link to='/' className="flex items-center gap-2 text-[22px] font-medium shrink-0">
+         <Link to='/'onClick={() =>window.scrollTo({top: 0,behavior: "smooth"})}
+            className="flex items-center gap-2 text-[22px] font-medium shrink-0">
             <BikeIcon size={24}/> Instacart
         </Link>
        </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.tsx';
+import ScrollToTop from './components/ScrollToTop.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <ScrollToTop />
     <CartProvider>
       <App />
     </CartProvider>
